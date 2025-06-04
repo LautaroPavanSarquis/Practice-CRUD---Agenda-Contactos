e9b511150d79e6f574ab88ba77eefa9cbcbfe184
Lauti <Lauti@DESKTOP-SUMD7P2>
2025-06-03 21:21:54 -0300

    Add project files.

---
+++ Practice Crud - Agenda Contactos/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Practice_Crud___Agenda_Contactos.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
