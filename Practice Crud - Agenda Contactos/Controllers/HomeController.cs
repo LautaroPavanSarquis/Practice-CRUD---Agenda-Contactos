5557348c5689a788d16a6df327d8cd2cf0ab6959
Lauti <Lauti@DESKTOP-SUMD7P2>
2025-06-16 21:56:24 -0300

    Finalizado el proyecto: agregado funcionalidad Edit, Detail y Delete en controlador y vistas

--- Practice Crud - Agenda Contactos/Controllers/HomeController.cs
+++ Practice Crud - Agenda Contactos/Controllers/HomeController.cs
@@ -43,6 +43,87 @@ namespace Practice_Crud___Agenda_Contactos.Controllers
             return View();
         }
 
+        [HttpGet]
+        public IActionResult Edit(int? id)
+        {
+            if (id == null )
+            {
+                return NotFound();
+            }
+
+            var contact = _context.Contact.Find(id);
+            if (contact == null) 
+            { 
+                return NotFound(); 
+            }
+
+            return View(contact);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public async Task<IActionResult> Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Update(contact);
+                await _context.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Detail(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = _context.Contact.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        [HttpGet]
+        public IActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var contact = _context.Contact.Find(id);
+            if (contact == null)
+            {
+                return NotFound();
+            }
+
+            return View(contact);
+        }
+
+        [HttpPost,ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteContact(int? id)
+        {
+            var contact = await _context.Contact.FindAsync(id);
+
+            if (contact == null)
+            {
+                return View();
+            }
+
+            _context.Contact.Remove(contact);
+            await _context.SaveChangesAsync();
+            return RedirectToAction("Index");   
+        }
+
         public IActionResult Privacy()
         {
             return View();
